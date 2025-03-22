000b1f588089f267b04a67e5c075d4292cafa0c8
Yuhao Chen <yuhaoc@zju.edu.cn>
2025-03-22 12:27:22 +0800

    Update test_graphvelo.py

--- tests/test_graphvelo.py
+++ tests/test_graphvelo.py
@@ -1,16 +1,3 @@
 ## test GraphVelo
-import dynamo as dyn
+from anndata import AnnData
 from graph_velocity import GraphVelo
-
-
-def test_graphvelo():
-    simulator = dyn.sim.BifurcationTwoGenes(dyn.sim.bifur2genes_params, tau=1)
-    simulator.simulate([0, 40], n_cells=2000)
-    adata = simulator.generate_anndata()
-    dyn.tl.neighbors(adata, basis='raw', n_neighbors=30)
-    gv = GraphVelo(adata, xkey='total', vkey='velocity_T', approx=False)
-    assert gv.X == adata.layers['total']
-    assert gv.V == adata.layers['velocity_T']
-    gv.train()
-    assert gv.T is not None
-    V_p = gv.project_velocity(adata.layers['total'])
